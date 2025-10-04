f5558ea554b1fd9ad4fef4a2166ee77912005269
jay Fallon <jayfallon@gmail.com>
2025-10-04 12:31:49 -0400

    playing with redis

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,12 +1,10 @@
 import Redis from 'ioredis';
 
-console.log('Redis environment variables:', {
-  REDIS_URL: process.env.REDIS_URL,
-  REDIS_PUBLIC_URL: process.env.REDIS_PUBLIC_URL,
-  REDISHOST: process.env.REDISHOST,
-  REDISPORT: process.env.REDISPORT,
+const redis = new Redis({
+  host: process.env.REDISHOST || 'redis',
+  port: parseInt(process.env.REDISPORT || '6379'),
+  password: process.env.REDISPASSWORD,
+  username: process.env.REDISUSER,
 });
-
-const redis = new Redis(process.env.REDIS_URL || '');
 
 export default redis;
